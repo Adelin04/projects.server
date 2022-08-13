9dc820fad1e2a38b2939d3897f85f339ea95e63f
Adelin <adelin.marin04@yahoo.com>
2022-08-13 13:38:59 +0300

    updated

--- _backend/Repository/DataBase.js
+++ _backend/Repository/DataBase.js
@@ -1,23 +1,3 @@
-// const mysql = require("mysql2");
-// const pool = mysql.createPool({
-//   host: "eu-cdbr-west-01.cleardb.com",
-//   user: "b5748a9b964b15",
-//   password: "94b7b847",
-//   database: "heroku_5cf8bc867f561e5",
-// });
-
-// module.exports = pool.promise();
-
-// const connection = mysql.createConnection({
-//   host: "eu-cdbr-west-01.cleardb.com",
-//   user: "b5748a9b964b15",
-//   password: "94b7b847",
-//   database: "heroku_5cf8bc867f561e5",
-// });
-// module.exports = connection;
-
-// Sequelize
-
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
   "heroku_5cf8bc867f561e5",
