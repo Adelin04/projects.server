f831e67bd7d73832e37758c4dd54f43f390ab605
Adelin04 <adelin.marin04@yahoo.com>
2022-09-28 17:27:02 +0300

    updated app

--- _frontend/src/components/_Utils/Dependency.js
+++ _frontend/src/components/_Utils/Dependency.js
@@ -1,3 +1,3 @@
-export const URL_HEROKU = "http://localhost:4000";
-// export const URL_HEROKU = "https://projects-app-node.herokuapp.com";
+// export const URL_HEROKU = "http://localhost:4000";
+export const URL_HEROKU = "https://projects-app-node.herokuapp.com";
 
